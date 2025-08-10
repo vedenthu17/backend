d3665bbbf9e5cc3cffa365dbecd8198c45c66d53
vedant.tamhanekar <vedanttamhanekar06@gmail.com>
2025-08-10 19:34:45 +0530

    initial commit

---
+++ blockingandnonbolcking.js
@@ -0,0 +1,18 @@
+
+// const fs =require("fs")
+// console.log('1')
+const os =require("os")
+console.log(os.cpus().length)
+// console.log(os)
+// // const  result=fs.readFileSync('./contacts.txt',"utf-8")
+// fs.readFile('./contacts.txt',"utf-8",(err,res)=>{
+//     if(err){
+//         console.log("error",err)
+//     }
+//     else{
+//         console.log(res)
+//     }
+// })
+// // console.log(result)
+ 
+// console.log("2")
